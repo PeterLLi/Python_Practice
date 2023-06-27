c74e3da500fb4005732e98fbd782c0a8591ef4bb
peterli <youngenli@gmail.com>
2023-06-27 17:29:29 +0800

    Added driver file. Changed logic for fill deck and printer methods.

---
+++ CS251_Practice/Program02/Driver.py
@@ -0,0 +1,7 @@
+import Program02_Deck
+
+
+class Driver:
+    program = Program02_Deck.MyDeck()
+    program.fill_deck()
+    program.printer()
--- CS251_Practice/Program02/Program02_Deck.py
+++ CS251_Practice/Program02/Program02_Deck.py
@@ -3,9 +3,10 @@ class MyDeck:
         self.deck = []
 
     def fill_deck(self):
-        self.deck = [True for i in range(52)]
+        for i in range(52):
+            self.deck[i] = True
 
     def printer(self):
         for i in range(len(self.deck)):
-            print(i)
+            print(self.deck[i])
 
