84c1c98f831a2fea8da81742d2ff73199a7eb0f6
peterli <youngenli@gmail.com>
2023-07-06 13:04:58 -0500

    Added constructor and init_deck to fill deck with all cards.

--- CS251_Practice/Program03/Cards.py
+++ CS251_Practice/Program03/Cards.py
@@ -1,4 +1,13 @@
+import itertools
+
+
 class Cards:
     suits = ('Spades', 'Diamonds', 'Hearts', 'Clubs')
     ranks = ('Ace', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King')
 
+    def __init__(self):
+        self.my_deck = []
+        self.init_deck()
+
+    def init_deck(self):
+        self.my_deck = list(itertools.product(Cards.ranks, Cards.suits))
