31644207d90d25048b6a6cb764658b439b832aa6
peterli <youngenli@gmail.com>
2023-07-06 13:23:19 -0500

    Removed '1' from ranks variable

--- CS251_Practice/Program03/Cards.py
+++ CS251_Practice/Program03/Cards.py
@@ -4,7 +4,7 @@ import random
 
 class Cards:
     suits = ('Spades', 'Diamonds', 'Hearts', 'Clubs')
-    ranks = ('Ace', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King')
+    ranks = ('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King')
 
     def __init__(self):
         self.my_deck = []
