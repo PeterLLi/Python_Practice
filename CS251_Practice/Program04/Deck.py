14594011cbd58fcfd5868e40538546e848e31601
peterli <youngenli@gmail.com>
2023-07-11 18:30:40 -0500

    Added class variables 'Suit' and 'Rank'. Added private method to populate deck

--- CS251_Practice/Program04/Deck.py
+++ CS251_Practice/Program04/Deck.py
@@ -1,7 +1,17 @@
+import itertools
+
+
 class Deck:
+    suits = ('Spades', 'Diamonds', 'Hearts', 'Clubs')
+    ranks = ('Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King')
+
     def __init__(self):
         self.deck = []
         self.cards_dealt = 0
+        self._init_deck()
+
+    def _init_deck(self):
+        self.deck = list(itertools.product(Deck.ranks, Deck.suits))
 
     def get_cards_dealt(self):
         return self.cards_dealt
@@ -19,3 +29,5 @@ class Deck:
 
     def collect_all_cards(self):
         self.cards_dealt = 0
+
+    def deal_card(self):
